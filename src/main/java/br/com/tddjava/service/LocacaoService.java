5570c83c146a232cdc1bf8b79c8e84f2c8c62373
alisoncruz <alisonn83@gmail.com>
2022-03-08 17:13:32 -0300

    criação de teste sem ferramenta, remoção de código desnecessário

--- src/main/java/br/com/tddjava/service/LocacaoService.java
+++ src/main/java/br/com/tddjava/service/LocacaoService.java
@@ -3,14 +3,16 @@ package br.com.tddjava.service;
 import br.com.tddjava.models.Filme;
 import br.com.tddjava.models.Locacao;
 import br.com.tddjava.models.Usuario;
+import br.com.tddjava.utils.DataUtils;
 
+import javax.naming.AuthenticationNotSupportedException;
 import java.util.Date;
 
 import static br.com.tddjava.utils.DataUtils.adicionarDias;
 
 public class LocacaoService {
 
-    public Locacao alugarFilme(Usuario usuario, Filme filme) throws Exception {
+    public Locacao alugarFilme(Usuario usuario, Filme filme) {
 
         Locacao locacao = new Locacao();
         locacao.setFilme(filme);
@@ -27,5 +29,21 @@ public class LocacaoService {
         // TODO adicionar mÃ©todo para salvar
 
         return locacao;
+    }
+
+    public static void main(String[] args) {
+        //cenário
+        LocacaoService service = new LocacaoService();
+        Usuario usuario = new Usuario("Usuario 1");
+        Filme filme = new Filme("Filme 1", 2, 5.0);
+
+        //ação
+        Locacao locacao = service.alugarFilme(usuario, filme);
+
+        //verificação
+        System.out.println(locacao.getValor() == 5.0);
+        System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(),new Date()));
+        System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(),DataUtils.obterDataComDiferencaDias(1)));
+
     }
 }
